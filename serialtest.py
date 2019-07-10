097553ae6e68ef22cae2ed03578d7dded8a26609
impure82 <52750684+impure82@users.noreply.github.com>
2019-07-10 16:26:20 +0200

    Add files via upload

---
+++ serialtest.py
@@ -0,0 +1,11 @@
+import serial
+import time
+port='/dev/ttyUSB0'
+
+ser = serial.Serial(port,9600)
+ser.isOpen()
+while 1:
+    ser.write(b"\n")
+    ser.write(b"Z")
+    ser.write(b"\r")
+    time.sleep(5)
